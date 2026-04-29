c1259dece7c48a099cf3f1ab3a18efc6bae7d83f
krypto <clayjeansen3@gmail.com>
2026-04-30 00:30:48 +0530

    remove password column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_29_184511) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_29_185249) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_29_184511) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name"
-    t.string "password"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
