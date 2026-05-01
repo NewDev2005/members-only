13acf167845715e7ea26cfd10ecb8c993587c1b0
krypto <clayjeansen3@gmail.com>
2026-05-02 04:00:09 +0530

    add destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,12 @@ class PostsController < ApplicationController
     @users = User.all
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.expect(post: [ :title, :body ])
